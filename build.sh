88d369b7bf408c91c844905fc6b3af996cb3fead
Avaer Kazmer <a@modules.io>
2018-10-03 23:22:22 -0400

    Update build script link order

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -g example.c -Inode_modules/native-video-deps/include -L/usr/lib/x86_64-linux-gnu/ -lm -lpthread -lavcodec -lavformat -lavutil -lswresample -lswscale
+g++ -g example.c -Inode_modules/native-video-deps/include -Lnode_modules/native-video-deps/lib/linux/ -lm -lpthread -lswscale -lavdevice -lavformat -lavcodec -lavutil
