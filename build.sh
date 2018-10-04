5be33dbc2e5638a6ff451d7d52c0f899558b21f6
Avaer Kazmer <a@modules.io>
2018-10-04 01:10:41 -0400

    Add missing libopus linkage in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -g example.c -Inode_modules/native-video-deps/include -Lnode_modules/native-video-deps/lib/linux/ -lm -lpthread -lswscale -lavdevice -lavformat -lavcodec -lavutil
+g++ -g example.c -Inode_modules/native-video-deps/include -Lnode_modules/native-video-deps/lib/linux/ -lm -lpthread -lswscale -lavdevice -lavformat -lavcodec -lavutil -lopus
